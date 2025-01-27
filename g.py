3bf524a260b92060589a21f329b517dfac605191
RedSanae <145702331+RedSanae@users.noreply.github.com>
2025-01-27 14:53:19 -0800

    Pushing

--- g.py
+++ g.py
@@ -1,13 +1,29 @@
 import datetime
+import time
+import os
 # adding date
 
-def add_expense(cat, date, money)
+def add_expense(category, date, money)
     try:
         date.datetime.striptime(date, '%Y-%m-%d' )
     except ValueError:
         print("Please try again with the format of YYYY-MM-DD")
         return
 
+def main()
+    cat=input("What category does this expense fall under?")
+    date=input("When did this expense occur?")
+    money=input("How much money did you waste on this expense?")
+
+    try:
+        add_expense(cat,date,money)
+    except:
+        print("One or more entries was invalid. Please try again")
+        time.sleep(1)
+        os.system('cls')
+        main()
+        
+main()
 
 
     
