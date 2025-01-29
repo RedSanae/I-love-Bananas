d8fb36aa36aa49638c376dbc644edd70c8e38056
RedSanae <145702331+RedSanae@users.noreply.github.com>
2025-01-29 14:49:22 -0800

    Update

--- g.py
+++ g.py
@@ -2,30 +2,61 @@ import datetime
 import time
 import os
 # adding date
-def add_expense(category, date, money)
 
-def add_expense(cat, date, money)
+expenses=[]
+
+
+def return_to_main():
+    time.sleep(.5)
+    os.system('clear')
+    expenses.sort()
+    main()
+
+def add_expenses(date, category, money):
+    cat=input("What category does this expense fall under?\n>")
+    date=input("When did this expense occur?\n>")
+    money=input("How much money did you waste on this expense?\n>")
+
+    exception=None
     try:
-        date.datetime.striptime(date, '%Y-%m-%d' )
-    except ValueError:
-        print("Please try again with the format of YYYY-MM-DD")
-        return
-
-def main()
-    cat=input("What category does this expense fall under?")
-    date=input("When did this expense occur?")
-    money=input("How much money did you waste on this expense?")
-
-    try:
-        add_expense(cat,date,money)
+        money=int(money)
     except:
-        print("One or more entries was invalid. Please try again")
-        time.sleep(1)
-        os.system('cls')
-        main()
-        
-main()
+        print("Invalid Money Input (Please only input numbers 0-9)")
+        exception=True
 
-
-
+    if not exception: 
+        expenses.append(add_expense(date,cat,money))
     
+    return_to_main()
+
+def show_expenses():
+    if len(expenses)>0:
+        totalAmount=0
+        for entry in expenses:
+            print(f"Date: {entry[0]} Category: {entry[1]} Amount: ${entry[2]}")
+            totalAmount+=int(entry[2])
+        print(f"\nTotal: ${totalAmount}")
+    else:
+        print("No expenses have been recorded yet.")
+    
+    return_to_main()
+
+def delete_expenses():
+    print("nothing")
+
+    return_to_main()
+
+def main():
+
+    options=input("[1]: Add an expense\n[2]: Show all expenses\n[3]: Delete an expense\n>")
+
+    if options=='1':
+        add_expenses()
+    if options=='2':
+        show_expenses()
+    if options=='3':
+        delete_expenses()
+    else:
+        print('please input a proper option')
+    
+main()
